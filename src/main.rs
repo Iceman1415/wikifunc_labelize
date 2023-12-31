ef0d85d58dffb8f479501b655fac757d1567175b
Iceman1415 <iceman1415@protonmail.com>
2023-12-31 22:27:16 +0800

    move index.html to its own file

--- src/main.rs
+++ src/main.rs
@@ -15,57 +15,7 @@ const DOMAIN: &str = "https://wikifunctions.beta.wmflabs.org/w";
 #[route("/", method = "GET", method = "POST")]
 async fn index() -> impl Responder {
     info!("index route");
-    HttpResponse::Ok().append_header(header::ContentType::html()).body(r#"<!doctype html>
-<html lang=en-US>
-<meta charset=utf8>
-<body>
-    <div><h1>Wikifunction Labelize - API for Making ZObjects Human Readable</h1></div>
-    <div><h2>Links</h2>
-        <span><a href="https://documenter.getpostman.com/view/15455445/VUxVq42m">Usage: Postaman documentation</a></span>
-        <span><a href="https://github.com/Iceman1415/wikifunc_labelize">Sourcecode</a></span>
-    </div>
-    <div><h2>GET /</h2><div>This help page</div></div>
-    <div><h2>POST /labelize</h2>
-        <div>Append human readable labels to all strings in the json body that are ZIDs (Zxxx) or Global Keys (ZxxxKyyy)</div>
-        <div>By default, the prefered languages of labels used are, in order: Japanese (Z1830), Chinese (Z1006), English (Z1002). This default value is to test and show that the language switching function works.</div>
-        <div>Alternatively you can supply your own order of prefered language in the POST body, like so: <code>{"data": "zobject...", "langs": ["Z1830", "Z1006", "Z1002"]}</code></div>
-    </div>
-    <div><h2>POST /compacify</h2>
-        <div>This tries to make the ZObject even more readable by simplifying its structure.</div>
-        <div>The main transformation we do is that we "raise" the type (Z1K1) of ZObjects (all ZObjects has its type in the key Z1K1) and the type in Arrays (all Arrays have the type as the first element) upwards, into the parent object's key.</div>
-        <div>In other words, we separate the type information from the rest of the data. The type information is merged into the key of objects instead.</div>
-        <div>Similarly, if an objects only have one key-value pair, the key is merged upward.</div>
-        <div>We also simplify commonly seen simple objects:<ul>
-            <li>String (Z6),</li>
-            <li>Reference (Z9),</li>
-            <li>Monolingual Text (Z11)</li>
-        </ul></div>
-        <div>A custom order of prefered language can be provided in the POST body, similar to /labelize</div>
-    </div>
-    <div><h2>Notes</h2>
-        <div><h3>Follow original HTTP Method</h3>
-            <div>When using Postman, POST requests seems to be converted into GET requests on toolforge. The request may then fail if the payload is too large for a GET request. This problem seems to be solved when I enabled the setting for "Redirect with the original HTTP method instead of the default behavior of redirecting with GET."</div>
-        </div>
-        <div><h3>Processing time</h3>
-            <div>The tool needs to fetch info about all the ZIDs mentioned in the request payload from wikifunction. Depending on the size of request, fetching all these information may take 10 seconds or more.</div>
-            <div>Fetched results are cached for 10 minutes, so there may be a delay between changes in wikifunction, and results returned from this tool.</div>
-        </div>
-        <div><h3>Feedback wanted</h3>
-            <div>This tool is still in active development (2022-09-05)</div>
-            <div>Please do contact me and provide feedback for:<ul>
-                <li>Unexpected output</li>
-                <li>Bugs and errors</li>
-                <li>feature suggestions</li>
-            </ul></div>
-        </div>
-    </div>
-    <div><h2>Contact</h2><ul>
-        <li>email: iceman1415@protonmail.com</li>
-        <li>wikimedia / phabricator / etc: Iceman1415</li>
-        <li>discord: Iceman#7876</li>
-    </ul></div>
-</body>
-</html>"#)
+    HttpResponse::Ok().append_header(header::ContentType::html()).body(include_str!("../static/index.html"))
 }
 
 mod simple_value;
